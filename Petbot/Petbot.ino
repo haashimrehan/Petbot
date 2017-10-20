6d3c1374a36e9757915e1d0a3757838b48c4b0ea
Haashim Rehan <haashimr@hotmail.com>
2017-10-20 15:26:44 -0400

    Ping
    Randomly runs 4 functions every single time the ping sensor detects an
    object.

--- Petbot/Petbot.ino
+++ Petbot/Petbot.ino
@@ -6,6 +6,7 @@ Servo rServo;
 const int trigPin = 2; //Ultrasonic Sensor
 const int echoPin = 4; //Ports
 
+
 void stopServos()             //Stops driving servos
 {
   rServo.writeMicroseconds(1500);
@@ -42,16 +43,9 @@ void leftCirc()
   rServo.writeMicroseconds(1800);
 }
 
-void setup() {
-  rServo.attach(9);  // attaches the servos
-  lServo.attach(10);
-  stopServos();
-  Serial.begin(9600);//initially stop servos
-  delay (2500);   //wait 2.5 seconds until main loop starts
-}
-
-void loop() {
+void ping() {
   //******Ping Sensor******//
+  int randNumber = random(1, 5); //# between 1 and 4 to indicate what it should do when it sees the sensor
   long duration, cm;
   pinMode(trigPin, OUTPUT);
   digitalWrite(trigPin, LOW);
@@ -71,18 +65,37 @@ void loop() {
   Serial.println();
   //******Ping Sensor******//
 
-  /*if (cm < 15)
-    {
-    if(random(1,3)==1){
+  if (cm < 15)
+  {
+    if (randNumber == 1) {
       turnLeft();
       delay(500);
-      } else {
-        turnRight();
-        delay(500);
-        }
-    } else {
+    } else if (randNumber == 2) {
+      turnRight();
+      delay(500);
+    } else if (randNumber == 3) {
+      leftCirc();
+      delay(2000);
+    } else if (randNumber == 4) {
+      rightCirc();
+      delay(2000);
+    }
+  } else {
     stopServos();
-    }*/
+  }
+
+}
+
+void setup() {
+  rServo.attach(9);  // attaches the servos
+  lServo.attach(10);
+  stopServos();
+  Serial.begin(9600);//initially stop servos
+  delay (2500);   //wait 2.5 seconds until main loop starts
+}
+
+void loop() {
+  ping();
 
 }
 
